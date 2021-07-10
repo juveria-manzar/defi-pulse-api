d2996450ea168f0607098b3f4f1e2df6e17f5782
Juveria Manzar <juveriamanzar29@gmail.com>
2021-07-10 19:26:37 +0530

    updated

--- src/controller/protocols.js
+++ src/controller/protocols.js
@@ -1,15 +1,16 @@
-const axios = require('axios')
+const axios = require('axios');
+const { response } = require('../server');
 
 
 //TODO add % percentage chnge
 exports.getProtocols = (req, res) => {
     axios.get('https://api.llama.fi/protocols')
         .then(response => {
-            let protocols=[]
-            response.data.forEach(protocol=>{
-                let formattedTVL=parseFloat(protocol.tvl).toPrecision(2)
-                formattedTVL=MoneyFormat(formattedTVL)
-                protocols[protocol.id]={
+            let protocols = []
+            response.data.forEach(protocol => {
+                let formattedTVL = parseFloat(protocol.tvl).toPrecision(2)
+                formattedTVL = MoneyFormat(formattedTVL)
+                protocols[protocol.id] = {
                     "name": protocol.name,
                     "chain": protocol.chain,
                     "category": protocol.category,
@@ -24,6 +25,30 @@ exports.getProtocols = (req, res) => {
         .catch(error => {
             console.log(error);
         });
+};
+
+exports.getTodaysTvl = (req, res) => {
+    axios.get("https://api.llama.fi/charts")
+        .then(response => {
+            let todaysTvl;
+            // let currDate= + new Date()
+            // console.log(currDate.toLocaleString())
+            let currDate = new Date().toLocaleString();
+            console.log(currDate.getDate() +"/" + (currDate.getMonth() + 1) +"/" + currDate.getFullYear())
+            response.data.forEach(protocol => {
+                let date = new Date(protocol.date * 1000).toLocaleString();
+                // let money = MoneyFormat(protocol.totalLiquidityUSD)
+                // money = parseFloat(money).toPrecision(2) + money.replace(/[^B|M|K]/g, "")
+                if (date == currDate) {
+                    todaysTvl.push({
+                        "date": date,
+                        "totalLiquidityUSD": protocol.totalLiquidityUSD
+                    })
+                    console.log(todaysTvl)
+                }
+            })
+            res.send(todaysTvl);
+        })
 };
 
 exports.getProtocolByName = (req, res) => {
@@ -46,13 +71,13 @@ function MoneyFormat(labelValue) {
         // Six Zeroes for Millions 
         : Math.abs(Number(labelValue)) >= 1.0e+6
 
-        ? Math.abs(Number(labelValue)) / 1.0e+6 + "M"
-        // Three Zeroes for Thousands
-        : Math.abs(Number(labelValue)) >= 1.0e+3
+            ? Math.abs(Number(labelValue)) / 1.0e+6 + "M"
+            // Three Zeroes for Thousands
+            : Math.abs(Number(labelValue)) >= 1.0e+3
 
-        ? Math.abs(Number(labelValue)) / 1.0e+3 + "K"
+                ? Math.abs(Number(labelValue)) / 1.0e+3 + "K"
 
-        : Math.abs(Number(labelValue));
+                : Math.abs(Number(labelValue));
 
 }
 
@@ -62,11 +87,11 @@ exports.getHistoryAll = (req, res) => {
             let obj = []
             response.data.forEach(protocol => {
                 let date = new Date(protocol.date * 1000).toLocaleString();
-                let money=MoneyFormat(protocol.totalLiquidityUSD)
-                money=parseFloat(money).toPrecision(2) + money.replace(/[^B|M|K]/g,"")
+                // let money = MoneyFormat(protocol.totalLiquidityUSD)
+                // money = parseFloat(money).toPrecision(2) + money.replace(/[^B|M|K]/g, "")
                 obj.push({
                     "date": date,
-                    "totalLiquidityUSD": money
+                    "totalLiquidityUSD": protocol.totalLiquidityUSD
                 })
             });
             res.send(obj);
--- src/routes/protocol.js
+++ src/routes/protocol.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProtocols, getProtocolByName, getProtocolTVL, getHistoryAll } = require("../controller/protocols");
+const { getProtocols, getProtocolByName, getProtocolTVL, getHistoryAll, getTodaysTvl } = require("../controller/protocols");
 
 router.get("/protocols", getProtocols);
+router.get("/protocols/tvl", getTodaysTvl);
 router.get("/protocol/:protocolName", getProtocolByName);
 router.get("/chart", getHistoryAll);
 router.get("/protocol/tvl/:protocolName", getProtocolTVL);
